b56849864fbb5991adda2ad2beb74457c2828fb4
balaganeshbaskar <balaganeshbaskar@gmail.com>
2022-03-20 18:05:30 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -201,3 +201,113 @@ function nav_active(page_name)
         
     }
 }
+
+
+function tabs_toggle(type)
+{   
+    document.getElementById("standard").hidden = true;
+    document.getElementById("balcony").hidden = true;
+    document.getElementById("deluxe").hidden = true;
+    document.getElementById("superior").hidden = true;
+    document.getElementById("grand_superior").hidden = true;
+
+    document.getElementById("standard_caret").hidden = true;
+    document.getElementById("balcony_caret").hidden = true;
+    document.getElementById("deluxe_caret").hidden = true;
+    document.getElementById("superior_caret").hidden = true;
+    document.getElementById("grand_superior_caret").hidden = true;
+    // background-image: linear-gradient(#6560b4, #9089e4);
+    // var c = "black";
+    // var d = "#212529";
+
+    // var a =  'linear-gradient(to right, #6560b4, #9089e4)';
+    // var b = "white";
+
+    // document.getElementById("customer_btn").style.backgroundColor = '';
+    // document.getElementById("customer_btn").style.color = b;
+    // document.getElementById("customer_btn").style.backgroundImage  = a;
+    // document.getElementById("invoice_btn").style.backgroundColor = '';
+    // document.getElementById("invoice_btn").style.color = b;
+    // document.getElementById("invoice_btn").style.backgroundImage  = a;
+    // document.getElementById("quotes_btn").style.backgroundColor = '';
+    // document.getElementById("quotes_btn").style.color = b;
+    // document.getElementById("quotes_btn").style.backgroundImage  = a;
+    // document.getElementById("dc_btn").style.backgroundColor = '';
+    // document.getElementById("dc_btn").style.color = b;
+    // document.getElementById("dc_btn").style.backgroundImage  = a;
+
+    if(type == 'standard')
+    {
+        // document.getElementById("customer").hidden = false;
+        // document.getElementById("invoice").hidden = true;
+        // document.getElementById("quotation").hidden = true;
+        // document.getElementById("dc").hidden = true;
+
+        // document.getElementById("customer_btn").style.backgroundColor = b;
+        // document.getElementById("customer_btn").style.backgroundImage  = '';
+        // document.getElementById("customer_btn").style.color = c;   
+
+        document.getElementById("standard").hidden = false;
+        document.getElementById("standard_caret").hidden = false;
+    }
+    else if(type == 'balcony')
+    {
+        // document.getElementById("customer").hidden = false;
+        // document.getElementById("invoice").hidden = true;
+        // document.getElementById("quotation").hidden = true;
+        // document.getElementById("dc").hidden = true;
+
+        // document.getElementById("customer_btn").style.backgroundColor = b;
+        // document.getElementById("customer_btn").style.backgroundImage  = '';
+        // document.getElementById("customer_btn").style.color = c;   
+
+        document.getElementById("balcony").hidden = false;
+        document.getElementById("balcony_caret").hidden = false;
+    
+    }
+    else if(type == 'deluxe')
+    {
+        // document.getElementById("customer").hidden = false;
+        // document.getElementById("invoice").hidden = true;
+        // document.getElementById("quotation").hidden = true;
+        // document.getElementById("dc").hidden = true;
+
+        // document.getElementById("customer_btn").style.backgroundColor = b;
+        // document.getElementById("customer_btn").style.backgroundImage  = '';
+        // document.getElementById("customer_btn").style.color = c;   
+
+        document.getElementById("deluxe").hidden = false;
+        document.getElementById("deluxe_caret").hidden = false;
+    
+    }
+    else if(type == 'superior')
+    {
+        // document.getElementById("customer").hidden = false;
+        // document.getElementById("invoice").hidden = true;
+        // document.getElementById("quotation").hidden = true;
+        // document.getElementById("dc").hidden = true;
+
+        // document.getElementById("customer_btn").style.backgroundColor = b;
+        // document.getElementById("customer_btn").style.backgroundImage  = '';
+        // document.getElementById("customer_btn").style.color = c;   
+
+        document.getElementById("superior").hidden = false;
+        document.getElementById("superior_caret").hidden = false;
+    
+    }
+    else if(type == 'grand_superior')
+    {
+        // document.getElementById("customer").hidden = false;
+        // document.getElementById("invoice").hidden = true;
+        // document.getElementById("quotation").hidden = true;
+        // document.getElementById("dc").hidden = true;
+
+        // document.getElementById("customer_btn").style.backgroundColor = b;
+        // document.getElementById("customer_btn").style.backgroundImage  = '';
+        // document.getElementById("customer_btn").style.color = c;   
+
+        document.getElementById("grand_superior").hidden = false;
+        document.getElementById("grand_superior_caret").hidden = false;
+    }
+
+}
